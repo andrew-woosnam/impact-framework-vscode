5813e6779916d130fdd34d067904f1974138d04f
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-30 19:01:12 -0400

    refactor: extract manifest creation to new file

---
+++ src/createManifestYaml.ts
@@ -0,0 +1,33 @@
+import * as vscode from "vscode";
+
+export async function tryCreateManifestYaml() {
+  const selectedFolders = await vscode.window.showOpenDialog({
+    canSelectFolders: true,
+    canSelectFiles: false,
+    openLabel: "Select Folder for manifest.yml",
+  });
+
+  if (!selectedFolders || selectedFolders.length === 0) {
+    vscode.window.showErrorMessage(
+      "No folder selected. Cannot create manifest.yml."
+    );
+    return;
+  }
+
+  const selectedFolder = selectedFolders[0];
+  const filePath = vscode.Uri.joinPath(selectedFolder, "manifest.yml");
+  try {
+    await vscode.workspace.fs.writeFile(filePath, Buffer.from(""));
+    vscode.window.showInformationMessage("Created manifest.yml successfully.");
+  } catch (error) {
+    if (error instanceof Error) {
+      vscode.window.showErrorMessage(
+        `Error creating manifest.yml: ${error.message}`
+      );
+    } else {
+      vscode.window.showErrorMessage(
+        "An unknown error occurred while creating manifest.yml."
+      );
+    }
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,15 @@
 import * as vscode from "vscode";
+import { tryCreateManifestYaml } from "./createManifestYaml";
 
 export function activate(context: vscode.ExtensionContext) {
   let createManifestCmd = vscode.commands.registerCommand(
     "impact-framework-vscode.createManifest",
     () => {
-      createManifestYaml();
+      tryCreateManifestYaml();
     }
   );
 
   context.subscriptions.push(createManifestCmd);
-
-  async function createManifestYaml() {
-    // Get the first workspace folder (if available)
-    const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-    if (!workspaceFolder) {
-      vscode.window.showErrorMessage("No workspace folder found.");
-      return;
-    }
-
-    // Create the "manifest.yml" file
-    const filePath = vscode.Uri.joinPath(workspaceFolder.uri, "manifest.yml");
-    try {
-      await vscode.workspace.fs.writeFile(filePath, Buffer.from(""));
-      vscode.window.showInformationMessage(
-        "Created manifest.yml successfully!"
-      );
-    } catch (error) {
-      if (error instanceof Error) {
-        vscode.window.showErrorMessage(
-          `Error creating manifest.yml: ${error.message}`
-        );
-      } else {
-        vscode.window.showErrorMessage(
-          "An unknown error occurred while creating manifest.yml."
-        );
-      }
-    }
-  }
 }
 
 export function deactivate() {}
