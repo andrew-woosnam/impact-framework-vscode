d83068bbdc6f9575fe88142e569bc0c1dae403eb
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-30 19:23:28 -0400

    feat: use generic schema for manifest.yml creation

--- src/createManifestCmd.ts
+++ src/createManifestCmd.ts
@@ -1,4 +1,6 @@
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
 export async function createManifestYamlAsync() {
   const selectedFolders = await vscode.window.showOpenDialog({
@@ -15,9 +17,20 @@ export async function createManifestYamlAsync() {
   }
 
   const selectedFolder = selectedFolders[0];
-  const filePath = vscode.Uri.joinPath(selectedFolder, "manifest.yml");
+  const newManifestPath = vscode.Uri.joinPath(selectedFolder, "manifest.yml");
+  const templatePath = path.join(
+    __dirname,
+    "..",
+    "static",
+    "manifest-template.yml"
+  );
+
   try {
-    await vscode.workspace.fs.writeFile(filePath, Buffer.from(""));
+    const templateContent = fs.readFileSync(templatePath, "utf8");
+    await vscode.workspace.fs.writeFile(
+      newManifestPath,
+      Buffer.from(templateContent)
+    );
     vscode.window.showInformationMessage("Created manifest.yml successfully.");
   } catch (error) {
     if (error instanceof Error) {
