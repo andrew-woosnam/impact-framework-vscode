3a87e896ea43e4792bafa54dca73c55cc92cd511
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-05-08 12:44:13 -0400

    feat: hover info for 'initialize' in manifests

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
 import { createManifestYamlAsync } from "./createManifestCmd";
+import fs from "fs";
+import path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   let createManifestCmd = vscode.commands.registerCommand(
@@ -10,6 +12,36 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(createManifestCmd);
+
+  // Register a hover provider for YAML
+  vscode.languages.registerHoverProvider("yaml", {
+    provideHover(document, position, token) {
+      const range = document.getWordRangeAtPosition(position);
+      const word = document.getText(range);
+
+      if (word === "initialize") {
+        try {
+          // Get the extension's directory path
+          const extensionPath = context.extensionPath;
+
+          // Read the content from initialize.md (use the correct path)
+          const markdownFilePath = path.join(
+            extensionPath,
+            "static/descriptions/initialize.md"
+          );
+          const markdownContent = fs.readFileSync(markdownFilePath, "utf8");
+
+          // Construct a custom hover message
+          const markdown = new vscode.MarkdownString(markdownContent);
+          markdown.isTrusted = true;
+
+          return new vscode.Hover(markdown);
+        } catch (error) {
+          console.error("Error reading or processing initialize.md:", error);
+        }
+      }
+    },
+  });
 }
 
 export function deactivate() {}
---
+++ static/descriptions/initialize.md
@@ -0,0 +1,22 @@
+**Initialize**
+
+The initialize section is where you define which plugins will be used in your manifest file and provide the global configuration for them. Below is sample for initialization:
+
+```yaml
+initialize:
+  plugins:
+    <PLUGIN-NAME-HERE>:
+      method: <METHOD-NAME-HERE>
+  outputs: ["csv", "yaml", "log"]
+```
+
+Where required values are:
+
+- `method`: the name of the function exported by the plugin.
+- `path`: the path to the plugin code. For example, for a plugin from our standard library installed from npm, this value would be `@grnsft/if-plugins`
+
+There is also an optional `global-config` field that can be used to set _global_ configuration that is common to a plugin wherever it is invoked across the entire manifest file.
+
+Impact Framework uses the `initialize` section to instantiate each plugin. A plugin cannot be invoked elsewhere in the manifest file unless it is included in this section.
+
+`outputs` is a list of possible export types (currently `csv`, `yaml`, and `log` are supported).
