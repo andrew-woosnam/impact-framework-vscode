03ece9d34571aeeadb66e0683733a92703bdbce9
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-05-17 16:40:35 -0400

    refactor: extract hoverProvider creation to file

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import { createManifestYamlAsync } from "./createManifestCmd";
-import fs from "fs";
-import path from "path";
+import { createManifestHoverProvider } from "./hoverProviderFactory";
 
 export function activate(context: vscode.ExtensionContext) {
   let createManifestCmd = vscode.commands.registerCommand(
@@ -13,42 +12,10 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(createManifestCmd);
 
-  // Register a hover provider for YAML
-  vscode.languages.registerHoverProvider("yaml", {
-    provideHover(document, position, token) {
-      const range = document.getWordRangeAtPosition(position);
-      if (range === null || range === undefined) {
-        return null;
-      }
-
-      // Extend the range by one character to check for ":"
-      const extendedRange = new vscode.Range(
-        range.start,
-        range.end.translate(0, 1)
-      );
-      const extendedWord = document.getText(extendedRange);
-      if (extendedWord.endsWith(":")) {
-        return tryGetInfoFromWord(extendedWord.slice(0, -1));
-      }
-
-      return null;
-    },
-  });
-
-  function tryGetInfoFromWord(word: string) {
-    try {
-      const extensionPath = context.extensionPath;
-      const markdownFilePath = path.join(
-        extensionPath,
-        `static/descriptions/${word}.md`
-      );
-      const markdownContent = fs.readFileSync(markdownFilePath, "utf8");
-      const markdown = new vscode.MarkdownString(markdownContent);
-      markdown.isTrusted = true;
-
-      return new vscode.Hover(markdown);
-    } catch (_) {}
-  }
+  vscode.languages.registerHoverProvider(
+    "yaml",
+    createManifestHoverProvider(context)
+  );
 }
 
 export function deactivate() {}
---
+++ src/hoverProviderFactory.ts
@@ -0,0 +1,43 @@
+import * as vscode from "vscode";
+import fs from "fs";
+import path from "path";
+
+export function createManifestHoverProvider(
+  context: vscode.ExtensionContext
+): vscode.HoverProvider {
+  return {
+    provideHover(document, position, _token) {
+      const range = document.getWordRangeAtPosition(position);
+      if (range === null || range === undefined) {
+        return null;
+      }
+
+      // Extend the range by one character to check for ":"
+      const extendedRange = new vscode.Range(
+        range.start,
+        range.end.translate(0, 1)
+      );
+      const extendedWord = document.getText(extendedRange);
+      if (extendedWord.endsWith(":")) {
+        return tryGetInfoFromWord(extendedWord.slice(0, -1), context);
+      }
+
+      return null;
+    },
+  };
+}
+
+function tryGetInfoFromWord(word: string, context: vscode.ExtensionContext) {
+  try {
+    const extensionPath = context.extensionPath;
+    const markdownFilePath = path.join(
+      extensionPath,
+      `static/descriptions/${word}.md`
+    );
+    const markdownContent = fs.readFileSync(markdownFilePath, "utf8");
+    const markdown = new vscode.MarkdownString(markdownContent);
+    markdown.isTrusted = true;
+
+    return new vscode.Hover(markdown);
+  } catch (_) {}
+}
