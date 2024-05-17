f0077831e01583c0f9ce309f75809e164aa8c6fa
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-05-17 17:16:06 -0400

    feat: add window listing built-in IF plugins (hardcoded)

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,24 @@
 import * as vscode from "vscode";
 import { createManifestYamlAsync } from "./createManifestCmd";
 import { createManifestHoverProvider } from "./hoverProviderFactory";
+import fs from "fs";
+import path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
+  const panel = vscode.window.createWebviewPanel(
+    "ifPluginsWindow", // Unique ID
+    "Impact Framework Plugins", // Title
+    vscode.ViewColumn.Beside, // Window location
+    {
+      enableScripts: true,
+    }
+  );
+
+  const extensionPath = context.extensionPath;
+  const htmlFilePath = path.join(extensionPath, `static/plugins.html`);
+  const htmlContent = fs.readFileSync(htmlFilePath, "utf8");
+  panel.webview.html = htmlContent;
+
   let createManifestCmd = vscode.commands.registerCommand(
     "impact-framework-vscode.createManifest",
     () => {
