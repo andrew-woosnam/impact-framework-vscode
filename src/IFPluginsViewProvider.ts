3f631c59095d4173bef1913accca8a79b17191ad
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-05-22 22:20:34 -0400

    refactor: extract class IFPluginsViewProvider to file

---
+++ src/IFPluginsViewProvider.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export class IFPluginsViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "ifPluginsView";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [vscode.Uri.joinPath(this._extensionUri, 'static')],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const styleUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        "static",
+        "styles.css"
+      )
+    );
+    const htmlUri = vscode.Uri.joinPath(
+      this._extensionUri,
+      "static",
+      "plugins.html"
+    );
+
+    let htmlContent = fs.readFileSync(htmlUri.fsPath, "utf8");
+    htmlContent = htmlContent.replace(/{{styleUri}}/g, styleUri.toString());
+
+    console.log(htmlContent);
+
+    return htmlContent;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 import { createManifestYamlAsync } from "./createManifestCmd";
 import { createManifestHoverProvider } from "./hoverProviderFactory";
-import fs from "fs";
+import { IFPluginsViewProvider } from "./IFPluginsViewProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   let createManifestCmd = vscode.commands.registerCommand(
@@ -18,55 +18,13 @@ export function activate(context: vscode.ExtensionContext) {
     createManifestHoverProvider(context)
   );
 
-  vscode.window.registerWebviewViewProvider(
-    "ifPluginsView",
-    new IFPluginsViewProvider(context.extensionUri)
+  const provider = new IFPluginsViewProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      IFPluginsViewProvider.viewType,
+      provider
+    )
   );
 }
 
 export function deactivate() {}
-
-class IFPluginsViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "ifPluginsView";
-
-  private _view?: vscode.WebviewView;
-
-  constructor(private readonly _extensionUri: vscode.Uri) {}
-
-  public resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
-
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [this._extensionUri],
-    };
-
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-  }
-
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    const styleUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(
-        this._extensionUri,
-        "static",
-        "styles.css"
-      )
-    );
-    const htmlUri = vscode.Uri.joinPath(
-      this._extensionUri,
-      "static",
-      "plugins.html"
-    );
-
-    let htmlContent = fs.readFileSync(htmlUri.fsPath, "utf8");
-    htmlContent = htmlContent.replace(/{{styleUri}}/g, styleUri.toString());
-
-    console.log(htmlContent);
-
-    return htmlContent;
-  }
-}
