50a8515bb290eb8dd33eafac3ff7fb5e87c322e5
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-06-24 14:50:32 -0400

    refactor createManifestCmd.ts

--- src/createManifestCmd.ts
+++ src/createManifestCmd.ts
@@ -1,8 +1,25 @@
+/** createManifestCmd.ts */
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
 export async function createManifestYamlAsync() {
+  const selectedFolder = await _selectFolder();
+  if (!selectedFolder) return;
+
+  const newManifestPath = vscode.Uri.joinPath(selectedFolder, 'manifest.yml');
+  const templatePath = _getTemplatePath();
+
+  try {
+    const templateContent = _readTemplate(templatePath);
+    await _writeManifest(newManifestPath, templateContent);
+    vscode.window.showInformationMessage('Created manifest.yml successfully.');
+  } catch (error) {
+    _handleError(error);
+  }
+}
+
+async function _selectFolder(): Promise<vscode.Uri | undefined> {
   const selectedFolders = await vscode.window.showOpenDialog({
     canSelectFolders: true,
     canSelectFiles: false,
@@ -13,34 +30,32 @@ export async function createManifestYamlAsync() {
     vscode.window.showErrorMessage(
       'No folder selected. Cannot create manifest.yml.',
     );
-    return;
+    return undefined;
   }
 
-  const selectedFolder = selectedFolders[0];
-  const newManifestPath = vscode.Uri.joinPath(selectedFolder, 'manifest.yml');
-  const templatePath = path.join(
-    __dirname,
-    '..',
-    'static',
-    'manifest-template.yml',
-  );
+  return selectedFolders[0];
+}
 
-  try {
-    const templateContent = fs.readFileSync(templatePath, 'utf8');
-    await vscode.workspace.fs.writeFile(
-      newManifestPath,
-      Buffer.from(templateContent),
+function _getTemplatePath(): string {
+  return path.join(__dirname, '..', 'static', 'manifest-template.yml');
+}
+
+function _readTemplate(templatePath: string): string {
+  return fs.readFileSync(templatePath, 'utf8');
+}
+
+async function _writeManifest(manifestPath: vscode.Uri, content: string) {
+  await vscode.workspace.fs.writeFile(manifestPath, Buffer.from(content));
+}
+
+function _handleError(error: unknown) {
+  if (error instanceof Error) {
+    vscode.window.showErrorMessage(
+      `Error creating manifest.yml: ${error.message}`,
     );
-    vscode.window.showInformationMessage('Created manifest.yml successfully.');
-  } catch (error) {
-    if (error instanceof Error) {
-      vscode.window.showErrorMessage(
-        `Error creating manifest.yml: ${error.message}`,
-      );
-    } else {
-      vscode.window.showErrorMessage(
-        'An unknown error occurred while creating manifest.yml.',
-      );
-    }
+  } else {
+    vscode.window.showErrorMessage(
+      'An unknown error occurred while creating manifest.yml.',
+    );
   }
 }
