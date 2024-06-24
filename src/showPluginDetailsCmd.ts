73f1d0c4992677ebffde1a36d81ba6365e84efb4
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-06-24 14:58:46 -0400

    refactor showPluginDetailsCmd.ts

--- src/showPluginDetailsCmd.ts
+++ src/showPluginDetailsCmd.ts
@@ -2,27 +2,6 @@
 import * as vscode from 'vscode';
 import { Plugin } from './types';
 
-function getPluginDetailsHtml(plugin: Plugin, styleUri: vscode.Uri): string {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Plugin Details</title>
-        <link rel="stylesheet" type="text/css" href="${styleUri}">
-    </head>
-    <body class="details-body">
-        <div class="details-title">${plugin.name}</div>
-        <div class="details-subtitle">by ${plugin.author}</div>
-        <div class="details-description">${plugin.description}</div>
-            ${generateTags(plugin.tags)}
-            ${generateStats(plugin.npmDownloads, plugin.githubStars)}
-            ${generateLinks(plugin.docs, plugin.github, plugin.npm)}
-        </div>
-    </body>
-    </html>`;
-}
-
 export async function showPluginDetails(
   plugin: Plugin,
   extensionUri: vscode.Uri,
@@ -41,49 +20,62 @@ export async function showPluginDetails(
   panel.webview.html = getPluginDetailsHtml(plugin, styleUri);
 }
 
-const generateTags = (tags?: string[]): string => {
+function getPluginDetailsHtml(plugin: Plugin, styleUri: vscode.Uri): string {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Plugin Details</title>
+        <link rel="stylesheet" type="text/css" href="${styleUri}">
+    </head>
+    <body class="details-body">
+        <div class="details-title">${plugin.name}</div>
+        <div class="details-subtitle">by ${plugin.author}</div>
+        <div class="details-description">${plugin.description}</div>
+        ${generateTags(plugin.tags)}
+        ${generateStats(plugin.npmDownloads, plugin.githubStars)}
+        ${generateLinks(plugin.docs, plugin.github, plugin.npm)}
+    </body>
+    </html>`;
+}
+
+function generateTags(tags?: string[]): string {
   if (!tags) return '';
   return `
-      <div class="details-tags-container">
-        ${tags
-          .map((tag) => `<span class="details-green-tag">${tag}</span>`)
-          .join('')}
-      </div>
-    `;
-};
+    <div class="details-tags-container">
+      ${tags
+        .map((tag) => `<span class="details-green-tag">${tag}</span>`)
+        .join('')}
+    </div>`;
+}
 
-const generateStats = (npmDownloads?: number, githubStars?: number): string => {
+function generateStats(npmDownloads?: number, githubStars?: number): string {
   if (!npmDownloads && !githubStars) return '';
   return `
-      <div class="details-stats">
-        ${
-          npmDownloads
-            ? `<div><strong>NPM Downloads:</strong> ${npmDownloads.toLocaleString()}</div>`
-            : ''
-        }
-        ${
-          githubStars
-            ? `<div><strong>GitHub Stars:</strong> ${githubStars.toLocaleString()}</div>`
-            : ''
-        }
-      </div>
-    `;
-};
+    <div class="details-stats">
+      ${
+        npmDownloads
+          ? `<div><strong>NPM Downloads:</strong> ${npmDownloads.toLocaleString()}</div>`
+          : ''
+      }
+      ${
+        githubStars
+          ? `<div><strong>GitHub Stars:</strong> ${githubStars.toLocaleString()}</div>`
+          : ''
+      }
+    </div>`;
+}
 
-const generateLinks = (
-  docs?: string,
-  github?: string,
-  npm?: string,
-): string => {
+function generateLinks(docs?: string, github?: string, npm?: string): string {
   return `
-      <div class="details-links">
-        ${docs ? `<a href="${docs}" target="_blank">Documentation</a>` : ''}
-        ${
-          github
-            ? `<a href="${github}" target="_blank">GitHub Repository</a>`
-            : ''
-        }
-        ${npm ? `<a href="${npm}" target="_blank">NPM</a>` : ''}
-      </div>
-    `;
-};
+    <div class="details-links">
+      ${docs ? `<a href="${docs}" target="_blank">Documentation</a>` : ''}
+      ${
+        github
+          ? `<a href="${github}" target="_blank">GitHub Repository</a>`
+          : ''
+      }
+      ${npm ? `<a href="${npm}" target="_blank">NPM</a>` : ''}
+    </div>`;
+}
